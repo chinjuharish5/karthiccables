cddb2bf0de595ac35b671cc460e5309548449253
Harish Kanakarajan <chinjuharish@gmail.com>
2018-01-20 21:40:34 +0530

    Kartchic Cables - Initial Commit

---
+++ left-sidebar.php
@@ -0,0 +1,130 @@
+<aside id="sidebar_left" class="nano nano-primary">
+		<div class="nano-content">
+			<div class="bg" style="z-index: 2; position: relative; width: 100%; min-height: 50px; padding: 5px 39px 4px; background: #fafafa; border-bottom: 1px solid #e0e0e0;">
+				<img src="https://ccrmcdn.iconwavetech.com/templates/yourlogohere.png" height="40px" width="120px"> 				
+			</div>
+				<!-- Start: Sidebar Header -->
+				<!--<header class="sidebar-header">
+						<div class="user-menu">
+								<div class="row text-center mbn">
+										<div class="col-xs-4">
+												<a href="dashboard.html" class="text-primary" data-toggle="tooltip" data-placement="top" title="Dashboard">
+														<span class="glyphicons glyphicons-home"></span>
+												</a>
+										</div>
+										<div class="col-xs-4">
+												<a href="pages_messages.html" class="text-info" data-toggle="tooltip" data-placement="top" title="Messages">
+														<span class="glyphicons glyphicons-inbox"></span>
+												</a>
+										</div>
+										<div class="col-xs-4">
+												<a href="pages_profile.html" class="text-alert" data-toggle="tooltip" data-placement="top" title="Tasks">
+														<span class="glyphicons glyphicons-bell"></span>
+												</a>
+										</div>
+										<div class="col-xs-4">
+												<a href="pages_timeline.html" class="text-system" data-toggle="tooltip" data-placement="top" title="Activity">
+														<span class="glyphicons glyphicons-imac"></span>
+												</a>
+										</div>
+										<div class="col-xs-4">
+												<a href="pages_profile.html" class="text-danger" data-toggle="tooltip" data-placement="top" title="Settings">
+														<span class="glyphicons glyphicons-settings"></span>
+												</a>
+										</div>
+										<div class="col-xs-4">
+												<a href="pages_gallery.html" class="text-warning" data-toggle="tooltip" data-placement="top" title="Cron Jobs">
+														<span class="glyphicons glyphicons-restart"></span>
+												</a>
+										</div>
+								</div>
+						</div>
+				</header>-->
+				<!-- End: Sidebar Header -->
+
+				<!-- sidebar menu -->
+				<ul class="nav sidebar-menu">
+						<!--<li class="sidebar-label pt20">Menu</li>
+						<li>
+								<a href="pages_calendar.html">
+										<span class="fa fa-calendar"></span>
+										<span class="sidebar-title">Calendar</span>
+								</a>
+						</li>
+						<li>
+								<a href="documentation/index.html">
+										<span class="glyphicons glyphicons-book_open"></span>
+										<span class="sidebar-title">Documentation</span>
+								</a>
+						</li>
+						<li class="active">
+								<a href="dashboard.html">
+										<span class="glyphicons glyphicons-home"></span>
+										<span class="sidebar-title">Dashboard</span>
+								</a>
+						</li>-->
+						<li class="sidebar-label pt15">Menu</li>
+						<li class="active">
+								<a href="dashboard.html">
+										<span class="glyphicons glyphicons-home"></span>
+										<span class="sidebar-title">Dashboard</span>
+								</a>
+						</li>						
+						<li>
+								<a class="accordion-toggle" href="#">
+										<span class="glyphicons glyphicons-fire"></span>
+										<span class="sidebar-title">Admin Plugins</span>
+										<span class="caret"></span>
+								</a>
+								<ul class="nav sub-nav">
+										<li>
+												<a href="admin_plugins-panels.html">
+														<span class="glyphicons glyphicons-book"></span> Admin Panels </a>
+										</li>
+										<li>
+												<a href="admin_plugins-modals.html">
+														<span class="glyphicons glyphicons-show_big_thumbnails"></span> Admin Modals </a>
+										</li>
+										<li>
+												<a href="admin_plugins-dock.html">
+														<span class="glyphicons glyphicons-sampler"></span> Admin Dock </a>
+										</li>
+								</ul>
+						</li>
+						<li>
+								<a class="accordion-toggle" href="#">
+										<span class="glyphicons glyphicons-cup"></span>
+										<span class="sidebar-title">Admin Forms</span>
+										<span class="caret"></span>
+								</a>
+								<ul class="nav sub-nav">
+										<li>
+												<a href="admin_forms-elements.html">
+														<span class="glyphicons glyphicons-edit"></span> Admin Elements </a>
+										</li>
+										<li>
+												<a href="admin_forms-widgets.html">
+														<span class="glyphicons glyphicons-calendar"></span> Admin Widgets </a>
+										</li>
+										<li>
+												<a href="admin_forms-layouts.html">
+														<span class="glyphicons glyphicons-more_windows"></span> Admin Layouts </a>
+										</li>
+										<li>
+												<a href="admin_forms-wizard.html">
+														<span class="glyphicons glyphicons-magic"></span> Admin Wizard </a>
+										</li>                            
+										<li>
+												<a href="admin_forms-validation.html">
+														<span class="glyphicons glyphicons-check"></span> Admin Validation </a>
+										</li>
+								</ul>
+						</li>
+				</ul>
+				<!--<div class="sidebar-toggle-mini">
+						<a href="#">
+								<span class="fa fa-sign-out"></span>
+						</a>
+				</div>-->
+		</div>
+</aside>
